cfc4496cce56aff35e12b9ccba9f6ae7d7f6d716
vipulshinu <vsuryavanshi43@gmail.com>
2018-02-22 22:50:31 -0500

    9:20

---
+++ onlinelibrary/onlinelibrary/src/com/javatpoint/DBIntializer.java
@@ -0,0 +1,8 @@
+package com.javatpoint;
+
+public interface DBIntializer {
+String DRIVER="com.mysql.jdbc.Driver";
+String CON_STRING="jdbc:mysql://localhost:8085/onlinelibrary\",\"admin\",\"admin\"";
+String USERNAME="admin";
+String PASSWORD="admin";
+}
